f36f15dd21547057c52ff94278970bc3e84a166d
Ramesh-DT <86780057+Ramesh-DT@users.noreply.github.com>
2021-07-26 16:18:28 +0530

    Add files via upload

---
+++ Game.py
@@ -0,0 +1,24 @@
+import random
+
+user_input = input("Enter a choice (rock, paper, scissors): ")
+possible_actions = ["rock", "paper", "scissors"]
+computer_input = random.choice(possible_actions)
+print(f"\nYou chose {user_input} and computer chose {computer_input}.\n")
+
+if user_input == computer_input:
+    print(f"Both players selected {user_input}. It's a tie!!!")
+elif user_input == "rock":
+    if computer_input == "scissors":
+        print("Rock smashes scissors! You win!!!")
+    else:
+        print("Paper covers rock! You lose.")
+elif user_input == "paper":
+    if computer_input == "rock":
+        print("Paper covers rock! You win!!!")
+    else:
+        print("Scissors cuts paper! You lose.")
+elif user_input == "scissors":
+    if computer_input == "paper":
+        print("Scissors cuts paper! You win!!!")
+    else:
+        print("Rock smashes scissors! You lose.")
