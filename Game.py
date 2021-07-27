a5db4da934fa8a20c7bbc2b35a5262a34d1d28fa
Ramesh-DT <86780057+Ramesh-DT@users.noreply.github.com>
2021-07-27 17:14:59 +0530

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,26 +1,39 @@
 import random
 
-user_input = input("Enter a choice (rock, paper, scissors): ")
-possible_actions = ["rock", "paper", "scissors"]
-computer_input = random.choice(possible_actions)
-print(f"\nYou chose {user_input} and computer chose {computer_input}.\n")
+print("rock, paper, scissors...")
 
-if user_input == computer_input:
-    print(f"Both players selected {user_input}. It's a tie!!!")
-elif user_input == "rock":
-    if computer_input == "scissors":
-        print("Rock smashes scissors! You win!!!")
+
+def play():
+    choices = ["rock", "paper", "scissors"]
+    computer = random.choice(choices)
+
+    player = input("Enter your choice: ").lower()
+    print(f"You chose {player} and computer chose {computer} ")
+
+    if player == computer:
+        print(f"Both players selected {player}. It's a tie!!!")
+    elif player == "rock":
+        if computer == "scissors":
+            print("Rock smashes scissors! You win!!!")
+        else:
+            print("Paper covers rock! You lose.")
+    elif player == "paper":
+        if computer == "rock":
+            print("Paper covers rock! You win!!!")
+        else:
+            print("Scissors cuts paper! You lose.")
+    elif player == "scissors":
+        if computer == "paper":
+            print("Scissors cuts paper! You win!!!")
+        else:
+            print("Rock smashes scissors! You lose.")
     else:
-        print("Paper covers rock! You lose.")
-elif user_input == "paper":
-    if computer_input == "rock":
-        print("Paper covers rock! You win!!!")
-    else:
-        print("Scissors cuts paper! You lose.")
-elif user_input == "scissors":
-    if computer_input == "paper":
-        print("Scissors cuts paper! You win!!!")
-    else:
-        print("Rock smashes scissors! You lose.")
-else:
-    print("Please give proper input")
+        print("Please give proper input")
+    play_again = input("Do you want to play again? yes or no: ").lower()
+    if play_again == "yes":
+        play()
+    elif play_again == "no":
+        print("Goodbye")
+
+
+play()
